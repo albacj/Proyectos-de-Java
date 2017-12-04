ebf8123845db13a9441ad6c44dad4a1475b606e1
Alba Carmona <albacarmona91@gmail.com>
2017-12-04 17:26:47 +0100

    Interfaz Persona.

---
+++ Persona/src/Persona.java
@@ -0,0 +1,23 @@
+
+public interface Persona {
+	
+	String getDNI();
+	void setDNI(String dni); //12345678A
+	
+	String getNombre();
+	void SetNombre(String nombre); //Alba
+	
+	String getApellidos();
+	void setApellidos(String apellidos); //Carmona Jamardo
+	
+	Integer getEdad();
+	void setEdad(Integer edad); //26
+	
+	Double getEstatura();
+	void setEstatura(Double estatura); //1,60
+	
+	Character getSexo();
+	void setSexo(Character sexo); //F (femenino)
+	
+
+}

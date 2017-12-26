2208aa6b02aabf4e7a893b91108a808ad22dab2f
DESKTOP-1CE6B53\Alba <albacarmona91@gmail.com>
2017-12-26 20:27:03 +0100

    Creados dos proyectos: Punto y Racional.

---
+++ Racional/src/racional/Racional.java
@@ -0,0 +1,16 @@
+package racional;
+
+public interface Racional {
+	
+	// Métodos consultores
+	int getNumerador();
+	int getDenominador();
+	
+	// Métodos modificadores
+	void setNumerador(int num);
+	void setDenominador(int den);
+	
+	//Devuelve el valor real obtenido al dividir el numerador entre el denominador
+	double getValor(int n, int d);
+
+}

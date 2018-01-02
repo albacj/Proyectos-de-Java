68b74539be575302afd732ed627cdc1c08a6d9bf
DESKTOP-1CE6B53\Alba <albacarmona91@gmail.com>
2018-01-02 19:02:20 +0100

    Implementados todos los métodos estáticos y sus pruebas.

--- Escritor/src/test/TestUtilidadesLibros.java
+++ Escritor/src/test/TestUtilidadesLibros.java
@@ -1,39 +1,63 @@
 package test;
 
+import librerias.tipos.*;
+import utilidades.Nacionalidades;
+import utilidades.UtilidadesLibros;
 
 public class TestUtilidadesLibros extends Test {
 	public static void main(String args[]) {
 		// TODO: Descomente estas líneas
-		// Libro l1 = new LibroImpl("Miguel", "de Cervantes y Saavedra",
-		// Nacionalidades.SPANISH, "ETSII", "Don Quijote de la Mancha",
-		// 1995);
-		// l1.setCopiasVendidas(14);
-		// l1.setPrecio(12.5);
+		Escritor e1 = new EscritorImpl("Miguel", "de Cervantes y Saavedra", Nacionalidades.SPANISH);
+		Libro l1 = new LibroImpl("Don Quijote de La Mancha", e1, 1995, "ETSII");
+		l1.setCopiasVendidas(14);
+		l1.setPrecio(12.5);
 		//
-		// Libro l2 = new LibroImpl("J. K.", "Rowling", Nacionalidades.BRITISH,
-		// "Salamandra", "Harry Potter y el cáliz de fuego", 2010);
-		// l2.setCopiasVendidas(57653);
-		// l2.setPrecio(19.95);
+		Escritor e2 = new EscritorImpl("J. K.", "Rowling", Nacionalidades.BRITISH);
+		Libro l2 = new LibroImpl("Harry Potter y el cáliz de fuego", e2, 2010, "Salamandra");
+		l2.setCopiasVendidas(57653);
+		l2.setPrecio(19.95);
 		//
-		// Libro l3 = new LibroImpl("Gabriel", "García Márquez",
-		// Nacionalidades.OTHER,
-		// "Mondadori", "Cien años de soledad", 1999);
-		// l3.setCopiasVendidas(0);
-		// l3.setPrecio(-19.95);
+		Escritor e3 = new EscritorImpl("Gabriel", "García Márquez", Nacionalidades.OTHER);
+		Libro l3 = new LibroImpl("Cien años de soledad", e3, 1999, "Mondadori");
+		l2.setCopiasVendidas(57653);
+		l2.setPrecio(19.95);
+		l3.setCopiasVendidas(0);
+		l3.setPrecio(-19.95);
 
 		mostrar("-------------------------APARTADO A----------------------------------");
+		
+		mostrar(UtilidadesLibros.esBestSeller(l1));
+		mostrar(UtilidadesLibros.esBestSeller(l2));
+		mostrar(UtilidadesLibros.esBestSeller(l3));
 
 		mostrar("-------------------------APARTADO B----------------------------------");
+		
+		mostrar(UtilidadesLibros.aplicaDescuento(l1, 0.1));
+		mostrar(UtilidadesLibros.aplicaDescuento(l2, 0.1));
 
 		mostrar("-------------------------APARTADO C----------------------------------");
+		
+		mostrar(UtilidadesLibros.aplicaIncremento(l1, 0.1));
+		mostrar(UtilidadesLibros.aplicaIncremento(l2, 0.1));
 
 		mostrar("-------------------------APARTADO D----------------------------------");
+		
+		mostrar(UtilidadesLibros.cambiaPrecio1(l1, 0.2, true));
+		mostrar(UtilidadesLibros.cambiaPrecio1(l2, 0.3, false));
 
 		mostrar("-------------------------APARTADO E----------------------------------");
+		
+		mostrar(UtilidadesLibros.cambiaPrecio2(l1, 0.4, 0));
+		mostrar(UtilidadesLibros.cambiaPrecio2(l2, 0.5, 569));
 
 		mostrar("-------------------------APARTADO F----------------------------------");
+		
+		mostrar(UtilidadesLibros.correctoPrecio(l1));
+		mostrar(UtilidadesLibros.correctoPrecio(l3));
 
 		mostrar("-------------------------APARTADO G----------------------------------");
+		
+		mostrar(UtilidadesLibros.corrigePrecio(l3));
 
 	}
 }
--- Escritor/src/utilidades/UtilidadesLibros.java
+++ Escritor/src/utilidades/UtilidadesLibros.java
@@ -1,7 +1,86 @@
 package utilidades;
 
+import librerias.tipos.Libro;
 
 public class UtilidadesLibros {
+	
 	public static Integer LIMITE_BEST_SELLER = 50000;
+	
+	public static Boolean esBestSeller(Libro l) {
+		
+		if(l.getCopiasVendidas() >= LIMITE_BEST_SELLER) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public static double aplicaDescuento(Libro l, double d) {
+		
+		return l.getPrecio() - (l.getPrecio() * d);
+		
+	}
+	
+	public static double aplicaIncremento(Libro l, double d) {
+		
+		return l.getPrecio() + (l.getPrecio() * d);
+		
+	}
+	
+	public static double cambiaPrecio1(Libro l, double d, boolean b) {
+		
+		double res = 0.0;
+		
+		if(b == true) {
+			res = aplicaDescuento(l,d);
+		}else {
+			res = aplicaIncremento(l, d);
+		}
+		
+		return res;
+		
+	}
+	
+	public static double cambiaPrecio2(Libro l, double d, int i) {
+		
+		double res = 0.0;
+		
+		switch(i) {
+		
+		case 0:
+			res = aplicaDescuento(l,d);
+			break;
+			
+		default:
+			res = aplicaIncremento(l,d);
+		
+		}
+		
+		return res;
+		
+	}
+	
+	public static boolean correctoPrecio(Libro l) {
+		
+		if(l.getPrecio() >= 0.0) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	
+	public static double corrigePrecio(Libro l) {
+		
+		double res = 0.0;
+		
+		if(l.getPrecio() <= 0.0) {
+			l.setPrecio(Double.NaN); 
+			res = l.getPrecio();
+		}
+		
+		return res;
+		
+	}
 
 }

d23834a52c2f4d6c361197bac1ed2c229025598c
DESKTOP-1CE6B53\Alba <albacarmona91@gmail.com>
2018-01-02 16:42:58 +0100

    Implementada la clase EscritorImpl.

---
+++ Escritor/src/librerias/tipos/EscritorImpl.java
@@ -0,0 +1,40 @@
+package librerias.tipos;
+
+public class EscritorImpl {
+	
+	// Atributos privados
+	private String nombre;
+	private String apellidos;
+	private String nacionalidad;
+
+	// Constructor
+	public EscritorImpl(String nom, String ape, String nac) {
+		// TODO Auto-generated constructor stub
+		nombre = nom;
+		apellidos = ape;
+		nacionalidad = nac;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getApellidos() {
+		return apellidos;
+	}
+	
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+	
+	public void setNacionalidad(String nac) {
+		nacionalidad = nac;
+	}
+	
+	public String toString() {
+		
+		String s = getApellidos() + ", " + getNombre() + " (" + getNacionalidad() + ")";
+		return s;
+	}
+
+}

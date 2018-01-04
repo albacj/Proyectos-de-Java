8b6df2402d0b8436404bf4703640622d8ba25fce
DESKTOP-1CE6B53\Alba <albacarmona91@gmail.com>
2018-01-04 16:25:39 +0100

    Creado un objeto nuevo de tipo Racional para posteriormente cambiar su denominador a 0 y comprobado que salta la excepción creada anteriormente.

--- RacionalBase/src/test/TestRacional.java
+++ RacionalBase/src/test/TestRacional.java
@@ -7,7 +7,10 @@ public class TestRacional extends Test{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Racional r1 = new RacionalImpl(564, 0);
+		//Racional r1 = new RacionalImpl(564, 0);
+		
+		Racional r = new RacionalImpl(4,87);
+		r.setDenominador(0);
 
 	}
 

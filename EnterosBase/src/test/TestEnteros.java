6dfb6851a5f3c1dbdf91f160c101af29d9be940f
DESKTOP-1CE6B53\Alba <albacarmona91@gmail.com>
2018-01-04 20:17:00 +0100

    En la clase TestEnteros se ha probado que el método se ha realizado de manera correcta.

---
+++ EnterosBase/src/test/TestEnteros.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Vector;
+import utiles.Enteros;
+
+public class TestEnteros extends Test{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vector<Integer> v = Enteros.iniVector(7);
+		mostrar("El contenido de v es " +v);
+
+	}
+
+}

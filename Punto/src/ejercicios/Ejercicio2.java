62d5257f60395cb3cde6ce7137fde0ad566f5bdb
DESKTOP-1CE6B53\Alba <albacarmona91@gmail.com>
2018-11-20 16:52:44 +0100

    Completo ejercicio de diferencia simétrica entre conjuntos.

--- Punto/src/ejercicios/Ejercicio2.java
+++ Punto/src/ejercicios/Ejercicio2.java
@@ -6,9 +6,22 @@ import java.util.Set;
 public class Ejercicio2 {
 	public static <T> Set<T> diferenciaSimetrica(Set<T> a, Set<T> b) {
 		// TODO Calculamos la unión de a y b
-
+		Set<T> union = new HashSet<T>();
+		union.addAll(a);
+		union.addAll(b);
 		// TODO Ahora la intersección:
-
+		Set<T> interseccion = new HashSet<T>();
+		
+		for(T elemento : a) {
+			if (b.contains(elemento)){
+				interseccion.add(elemento);
+			}
+		}
+		
 		// TODO La diferencia simétrica será la unión menos la intersección:
+		Set<T> solucion = new HashSet<T>();
+		solucion.addAll(union);
+		solucion.removeAll(interseccion);
+		return solucion;
 	}
 }

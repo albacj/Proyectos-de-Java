1f987ea3c1cb3f5070c95780d89a1cef81be4fcd
DESKTOP-1CE6B53\Alba <albacarmona91@gmail.com>
2018-11-20 19:31:59 +0100

    Ejercicio intercambia elementos sobre listas.

--- Punto/src/ejercicios/Ejercicio5.java
+++ Punto/src/ejercicios/Ejercicio5.java
@@ -11,5 +11,16 @@ public class Ejercicio5 extends Test {
 
 	public static <T> void intercambia(List<T> l1, List<T> l2) {
 		// TODO
+		for(T elemento : l1) {
+			if(Collections.frequency(l1, elemento) > 1) {
+				if(!l2.contains(elemento)) {
+					l2.add(elemento);
+				}
+			}
+			/**
+			 * Pendiente eliminar el elemento repetido de l1 
+			 * Cuando hago remove, no le gusta a eclipse
+			*/
+		}
 	}
 }

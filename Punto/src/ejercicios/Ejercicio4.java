0fcb97921d452f9f3183fe49a74f163c0708148d
DESKTOP-1CE6B53\Alba <albacarmona91@gmail.com>
2018-11-20 17:31:46 +0100

    Ejercicio candidatos idioma completo.

--- Punto/src/ejercicios/Ejercicio4.java
+++ Punto/src/ejercicios/Ejercicio4.java
@@ -10,21 +10,62 @@ public class Ejercicio4 extends Test {
 	private static Set<String> alMenosDos = null;
 	private static Set<String> soloUno = null;
 
-	public static Set<String> apartadoA(Set<String> ingles,
+	public static Set<String> apartadoA(Set<String> ingles, 
 			Set<String> frances, Set<String> aleman) {
 		// Superaron inglés y otro:
 		// TODO
+		inglesYOtro = new HashSet<String>();
+		
+		for(String cIngles : ingles) {
+			if(frances.contains(cIngles) || aleman.contains(cIngles)) {
+				inglesYOtro.add(cIngles);
+			}
+		}
+		
+		return inglesYOtro;
 	}
 
 	public static Set<String> apartadoB(Set<String> ingles,
 			Set<String> frances, Set<String> aleman) {
 		// Superaron al menos dos idiomas. Suponemos calculado inglesYOtro
 		// TODO	
+		alMenosDos = new HashSet<String>();
+		
+		for(String cIngles : ingles) {
+			if(frances.contains(cIngles)) {
+				alMenosDos.add(cIngles);
+			}else if(aleman.contains(cIngles)) {
+				alMenosDos.add(cIngles);
+			}
+		}
+		
+		for(String cFrances : frances) {
+			if(aleman.contains(cFrances)) {
+				alMenosDos.add(cFrances);
+			}
+		}
+		
+		return alMenosDos;
 	}
 
 	public static Set<String> apartadoC(Set<String> ingles,
 			Set<String> frances, Set<String> aleman) {
 		// Sólo dominan un idioma. Suponemos calculado alMenosDos
 		// TODO
+		soloUno = new HashSet<String>();
+		
+		for(String cIngles : ingles) {
+			if (!frances.contains(cIngles) && !aleman.contains(cIngles)) {
+				soloUno.add(cIngles);
+			}
+		}
+		
+		for(String cFrances : frances) {
+			if(!aleman.contains(cFrances)) {
+				soloUno.add(cFrances);
+			}
+		}
+		
+		return soloUno;
 	}
 }

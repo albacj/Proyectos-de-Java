34e10ae09da29a36ff78b169786d5da9d8a6be5a
DESKTOP-1CE6B53\Alba <albacarmona91@gmail.com>
2018-11-21 21:00:21 +0100

    Ejercicios de personas completado.

--- TratamientosSecuenciales1/src/resolucionPractica/Personas.java
+++ TratamientosSecuenciales1/src/resolucionPractica/Personas.java
@@ -1,5 +1,7 @@
 package resolucionPractica;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -10,29 +12,73 @@ public class Personas {
 	////////////////////////////////////////////////////////////////	
 	public static Double masAlto(List<Persona> grupo){
 
-		return null;
+		double altura = grupo.get(0).getEstatura();
+		
+		for(Persona p : grupo) {
+			
+			if(!(p.getEstatura() < altura)) {
+				altura = p.getEstatura();
+			}
+			
+		}
+		
+		return altura;
 	}
 
 	////////////////////////////////////////////////////////////////	
 	public static Boolean todosMayoresQue(Set<Persona> grupo, Integer edad){
 		
-		return null;
+		boolean mayores = false;
+		
+		for(Persona p : grupo) {
+			
+			if(p.getEdad() < edad) {
+				break;
+			}else {
+				mayores = true;
+			}
+			
+		}
+		
+		return mayores;
 	}
 
 	////////////////////////////////////////////////////////////////
 	public static Double edadMedia(Set<List<Persona>> red){
 		
-		return null;
+		double media = 0.0;
+		
+		for (Persona p : agrupaATodos(red)) {
+			media = media + p.getEdad();
+		}
+		
+		return media / agrupaATodos(red).size();
 	}
+	
+	/** Método auxiliar para agrupar todos los set en uno solo */
 	
 	private static Set<Persona> agrupaATodos(Set<List<Persona>> red){
 		
-		return null;
+		Set<Persona> agrupadas = new HashSet<Persona>();
+		
+		for(List<Persona> lp : red) {
+			agrupadas.addAll(lp);
+		}
+		
+		return agrupadas;
+		
 	}
+	
 	////////////////////////////////////////////////////////////////	
 	public static List<String> listadoDNI(Set<Persona> grupo){
 		
-		return null;
+		List<String> dnis = new ArrayList<String>();
+		
+		for(Persona p : grupo) {
+			dnis.add(p.getDNI());
+		}
+		
+		return dnis;
 	}
 
 }

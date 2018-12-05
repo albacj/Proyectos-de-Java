82765f31cab6b8f155fa5115c70e147c22821175
DESKTOP-1CE6B53\Alba <albacarmona91@gmail.com>
2018-12-05 14:01:47 +0100

    Análisis de rendimiento de listas completado.

--- FactoriasEIterables/src/test/PerformanceListsTest.java
+++ FactoriasEIterables/src/test/PerformanceListsTest.java
@@ -3,22 +3,29 @@ package test;
 import java.util.List;
 import java.util.Random;
 
-import edu.use.lsi.fp.types.ListType;
-import edu.use.lsi.fp.utils.Lists;
-
 public class PerformanceListsTest extends Test {
 
-	// TODO: Ejercicio 2.a
+	// Ejercicio 2.a
 	// a) En la clase ListsPerformanceTest cree las siguiente constantes.
 
 	private static Random rand = new Random();
 	private static long tBefore, tAfter;
+	
+	private static final int NUM_INSERTS = 1000000;
+	private static final int NUM_SEARCHES = 5000;
+	private static final int NUM_DELETES = 5000;
+	private static final int NUM_ACCESSES = 5000;
+	private static final int MAX_VALUE = 100000;
 
 	public static void main(String[] args) {
 
-		// TODO: Ejercicio 2.b
+		// Ejercicio 2.b
 		// b) Usando la factoría cree tres listas llamadas arrayList, vector y
 		// linkedList.
+		
+		List arrayList = utils.Lists.create(types.ListType.ARRAY_LIST);
+		List vector = utils.Lists.create(types.ListType.VECTOR_LIST);
+		List linkedList = utils.Lists.create(types.ListType.LINKED_LIST);
 
 		show("TEST DE INSERCIÓN=============================");
 		show("Vamos a insertar " + NUM_INSERTS + " elementos...");
@@ -43,29 +50,35 @@ public class PerformanceListsTest extends Test {
 
 	}
 
-	// TODO: Ejercicio 2.c
+	// Ejercicio 2.c
 	// c) Termine de implementar el método insertTest(l) para que ejecute un
 	// bucle que inserte en la lista l tantos números enteros aleatorios entre 0
 	// y MAX_VALUE como indique la constante NUM_INSERTS. Para generar un número
 	// entero aleatorio entre 0 y MAX_VALUE utilice el siguiente código.
 	private static void insertTest(List<Integer> l) {
 		tBefore = System.currentTimeMillis();
-		for () {
-			
+		
+		for (int i = 0; i < NUM_INSERTS; i++) {
+			Random r = new Random(); //Crea un objeto de la clase Random
+			Integer x = r.nextInt(MAX_VALUE); //Asigna a x un valor aleatorio
+			l.add(x);
 		}
+		
 		tAfter = System.currentTimeMillis();
 		show(l.getClass().getName() + ":\t" + (tAfter - tBefore)
 				+ " milisegundos");
 	}
 
-	// TODO: Ejercicio 2.d
+	// Ejercicio 2.d
 	// d) Implemente el método searchTest(l) para que muestre el tiempo que
 	// tarda en ejecutarse un bucle en el que se buscan (con el método
 	// contains()) tantos elementos como indique la constante NUM_SEARCHES.
 	private static void searchTest(List<Integer> l) {
 		tBefore = System.currentTimeMillis();
-		for () {
-			
+		for (int i = 0; i < NUM_SEARCHES; i++) {
+			Random r = new Random(); //Crea un objeto de la clase Random
+			Integer x = r.nextInt(MAX_VALUE); //Asigna a x un valor aleatorio
+			l.contains(x);
 		}
 		tAfter = System.currentTimeMillis();
 		show(l.getClass().getName() + ":\t" + (tAfter - tBefore)
@@ -73,16 +86,15 @@ public class PerformanceListsTest extends Test {
 
 	}
 
-	// TODO: Ejercicio 2.e
+	// Ejercicio 2.e
 	// e) Implemente el método accessTest(l) para que muestre el tiempo que
 	// tarda en ejecutarse un bucle en el que se acceden (con el método get()) a
 	// tantos elementos aleatorios entre 0 y NUM_INSERTS como indique la
 	// constante NUM_ACCESSES.
 	private static void accessTest(List<Integer> l) {
 		tBefore = System.currentTimeMillis();
-		Integer n = 0;
-		for () {
-			
+		for (int i = 0; i < NUM_ACCESSES; i++) {
+			l.get(i);
 		}
 		tAfter = System.currentTimeMillis();
 		show(l.getClass().getName() + ":\t" + (tAfter - tBefore)
@@ -90,13 +102,14 @@ public class PerformanceListsTest extends Test {
 
 	}
 
-	// TODO: Ejercicio 2.f
+	// Ejercicio 2.f
 	// f) Implemente el método deleteTest(l) para que muestre el tiempo que
 	// tarda en ejecutarse un bucle en el que se eliminan (con el método
 	// remove()) los NUM_DELETES primeros elementos de la lista.
 	private static void deleteTest(List<Integer> l) {
 		tBefore = System.currentTimeMillis();
-		for () {
+		for (int i = 0; i < NUM_DELETES; i++) {
+			l.remove(i);
 		}
 		tAfter = System.currentTimeMillis();
 		show(l.getClass().getName() + ":\t" + (tAfter - tBefore)
